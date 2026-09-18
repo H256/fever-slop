758eb4ac7db3b28e69296901e357a395b5e3d90a
H256 <hansi256@web.de>
2026-09-18 15:00:19 +0200

    WIP: #1064 per-family boundary spec registry (H3/LTX/MSR; krea=non-workflow)

--- tests/suites/unit.py
+++ tests/suites/unit.py
@@ -206,6 +206,7 @@ UNIT_MODULES = (
     "vocal_timeline_analyzer",
     "workflow_asset_name_replacement",
     "workflow_benchmark",
+    "workflow_import_families",
     "workflow_import_inspector",
     "workflow_materializer",
     "workflow_patch_spec",
